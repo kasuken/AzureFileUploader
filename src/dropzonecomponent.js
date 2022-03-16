34549a7ae7041610b8fc745981a46d52f1d47f3e
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-03-16 09:18:35 +0100

    found a better dropzone component

--- src/dropzonecomponent.js
+++ src/dropzonecomponent.js
@@ -1,82 +1,30 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
+import 'react-dropzone-uploader/dist/styles.css'
+import Dropzone from 'react-dropzone-uploader'
 
-const baseStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  padding: '20px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#eeeeee',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  transition: 'border .3s ease-in-out'
-};
-
-const activeStyle = {
-  borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-  borderColor: '#00e676'
-};
-
-const rejectStyle = {
-  borderColor: '#ff1744'
-};
-
-function DropzoneComponent(props) {
-  const [files, setFiles] = useState([]);
-
-  const onDrop = useCallback(acceptedFiles => {
-    setFiles(acceptedFiles.map(file => Object.assign(file, {
-      preview: URL.createObjectURL(file)
-    })));
-  }, []);
-
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject
-  } = useDropzone({
-    onDrop
-  });
-
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isDragActive ? activeStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isDragActive,
-    isDragReject,
-    isDragAccept
-  ]);
-
-  const uploadFiles = files.map(file => (
-    <div key={file.name}>
-      <p>{file.name}</p>
-    </div>
-  ));
-
-  useEffect(() => () => {
-    files.forEach(file => URL.revokeObjectURL(file.preview));
-  }, [files]);
+const DropzoneComponent = () => {
+  const getUploadParams = ({ meta }) => { 
+    
+    console.log(meta);
+    
+    return { url: 'https://httpbin.org/post' } 
+  
+  }
+  
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
+  
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+  }
 
   return (
-    <section>
-      <div {...getRootProps({style})}>
-        <input {...getInputProps()} />
-        <div>Drag and drop your files here.</div>
-      </div>
-      <aside>
-        {uploadFiles}
-      </aside>
-    </section>
+    <Dropzone
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+    />
   )
 }
 
