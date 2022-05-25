1a5899710f879bb92f43c4485ae75ea8d4fa4ac9
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2022-05-25 06:34:20 +0200

    Update index.js

--- api/Uploader/index.js
+++ api/Uploader/index.js
@@ -6,28 +6,32 @@ const multipart = require("parse-multipart");
 module.exports = async function (context, req) {
     context.log('Uploader trigger function processed a request.');
 
-    const bodyBuffer = Buffer.from(req.body);
+    try {
+        const bodyBuffer = Buffer.from(req.body);
 
-    const boundary = multipart.getBoundary(req.headers['content-type']);
-    const parts = multipart.Parse(bodyBuffer, boundary);
-    const res = { body: { name: parts[0].filename, type: parts[0].type, data: parts[0].data.length } };
+        const boundary = multipart.getBoundary(req.headers['content-type']);
+        const parts = multipart.Parse(bodyBuffer, boundary);
+        const res = { body: { name: parts[0].filename, type: parts[0].type, data: parts[0].data.length } };
 
-    const blobServiceClient = BlobServiceClient.fromConnectionString(
-        process.env.StorageConnectionString
-    );
+        const blobServiceClient = BlobServiceClient.fromConnectionString(
+            process.env.StorageConnectionString
+        );
 
-    const containerName = "container" + uuidv1();;
+        const containerName = "container" + uuidv1();;
 
-    const containerClient = blobServiceClient.getContainerClient(containerName);
-    await containerClient.create();
+        const containerClient = blobServiceClient.getContainerClient(containerName);
+        await containerClient.create();
 
-    containerClient.setAccessPolicy('blob');
+        containerClient.setAccessPolicy('blob');
 
-    const blobName = res.body.name;
+        const blobName = res.body.name;
 
-    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    await blockBlobClient.upload(parts[0].data, parts[0].data.length);
+        await blockBlobClient.upload(parts[0].data, parts[0].data.length);
+    } catch (error) {
+        console.log(error);
+    }
 
     context.res = {
         status: 200,
